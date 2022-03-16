df87fd39c6ae09adcd313ffd53627101063df796
L0c <locinone@protonmail.com>
2022-03-16 13:31:58 +0100

    terminal

---
+++ termY/shell/dir1/echo.c
@@ -0,0 +1,193 @@
+#include "echo.h"
+#include "builtin.h"
+/**
+** @brief               Gets the options
+** @param argv          Array of string arguments.
+** @param opt           Options structure.
+** @param argc          The number of arguments.
+*/
+void getOptions(char** argv, Options* opt, size_t argc)
+{
+    size_t i, j = 0;//1 
+    for (i = 0; i < argc; i++) //1
+    {
+        if (argv[i][0] == '-')
+        {
+            j = 1;
+            while (argv[i][j] == 'e' || argv[i][j] == 'n' || argv[i][j] == 'E')
+            {
+                if (argv[i][j] == 'n')
+                    opt->nflag = true;
+                if (argv[i][j] == 'e')
+                    opt->nflag = true;
+                if (argv[i][j] == 'E')
+                    opt->nflag = true;
+
+                j++;
+            }
+
+            if ((argv[i][j] != '\0') || (j == 1))
+                break;
+        }
+        else
+            break;
+  }
+  opt->ind = i;
+}
+
+/**
+** @brief               Figures out if a character is escaped.
+** @param c             Character.
+** @return              Returns true if character is escaped.
+*/
+bool isEscaped(char c)
+{
+    char esc[] = {'a', 'b', 'c', 'e', 'f', 'n', 'r', 't', 'v', '\\'};
+    int i = 0;
+    while (i < 10 && esc[i] != c)
+        i++;
+    
+    if (i != 10)
+        return true;
+    return false;
+}
+
+/**
+** @brief               Prints escaped characters.
+** @param c             Character.
+** @param builtinFd     Files.
+** @return              Returns -1 in case of error.
+*/
+int printEscaped(char c, BuiltinFd *builtinFd)
+{
+    switch (c)
+    {
+        case 'c':
+            return -1;
+        case 'e':
+            fprintf(builtinFd->out, "%s", "\033");
+            return 0;
+        default:
+            fprintf(builtinFd->out, "\\%c", c);
+            return 0;
+    }
+}
+
+/**
+** @brief               Prints a word (string seperated with spaces).
+** @param arg           Character array containing the "word".
+** @param builtinFd     Files.
+** @return              Returns -1 in case of error.
+*/
+int eprintWord(char* arg, BuiltinFd *builtinFd)
+{
+    for (size_t i = 0; arg[i] != '\0'; i++)
+    {
+        if (arg[i] == '\\')
+        {
+            if (arg[i] != '\0')
+            {
+                if (isEscaped(arg[i+1]))
+                {
+                    if (printEscaped(arg[i+1], builtinFd) == -1)
+                    {
+                        fflush(builtinFd->out);
+                        return -1;
+                    }
+                }
+                else
+                {
+                    fprintf(builtinFd->out, "%c", arg[i]);//+1]);
+                }
+                i++;
+            }
+        }
+        else
+            fprintf(builtinFd->out, "%c", arg[i]);//i+1]);
+    }
+    fflush(builtinFd->out);
+    return 0;
+}
+
+/**
+** @brief               Prints the whole "word array".
+** @param argv          Array of strings.
+** @param argc          Number of "words" in the array.
+** @param opt           Struct options defining options.
+** @param builtinFd     Files.
+** @return              Returns -1 in case of error.
+*/
+int eprintAll(char** argv, size_t argc, Options opt, BuiltinFd *builtinFd)
+{
+    size_t i;
+
+    if (opt.eflag == false)
+    {
+        for (i = opt.ind; i < argc - 1; i++)
+            fprintf(builtinFd->out, "%s ", argv[i]);
+        if (argv[i])
+            fprintf(builtinFd->out, "%s", argv[i]);
+    }
+    else
+    {
+        for (i = opt.ind ; i < argc-1; i++)
+        {
+            if (eprintWord(argv[i], builtinFd) == -1)
+                return -1;
+            fprintf(builtinFd->out, " ");
+        }
+
+        if (argv[i])
+        {
+            if (eprintWord(argv[i], builtinFd) == -1)
+                return -1;
+        }
+    }
+
+    return 0;
+}
+
+/**
+** @brief               Echo main function.
+** @param argv          Array of string arguments.
+** @param builtinFd     Files.
+** @return              Returns 0 in case of success, else -1.
+*/
+int echo(char** argv, BuiltinFd *builtinFd)
+{
+    struct Options opt;
+    opt.nflag = false;
+    opt.ind = 0;
+
+    size_t argc = getArgc(argv);
+    if (argc != 0)
+    {
+        getOptions(argv, &opt, argc);
+
+        if (eprintAll(argv, argc, opt, builtinFd) == -1)
+            return -1;
+    }
+
+    if (opt.nflag == false)
+        fprintf(builtinFd->out, "\n");
+    
+    fflush(builtinFd->out);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc){}
+    struct builtinFd *terminal = NULL;
+    terminal = (struct builtinFd *) malloc(sizeof(struct builtinFd));
+    terminal->in = stdin;//STDIN_FILENO;
+    terminal->out = stdout;//(FILE *) STDOUT_FILENO;
+    terminal->err = stderr;//(FILE *) STDERR_FILENO;
+    terminal->inNo =  STDIN_FILENO;
+    terminal->outNo = STDOUT_FILENO;
+    terminal->errNo = STDOUT_FILENO;
+    echo(argv,terminal);
+    free(terminal);
+}
+
+
---
+++ termY/shell/main.c
@@ -0,0 +1,132 @@
+#include <unistd.h> 
+#include "bsh.h"
+
+
+char *lsh_read_line(void)
+{
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer) {
+    fprintf(stderr, "bsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    // Read a character
+    c = getchar();
+
+    // If we hit EOF, replace it with a null character and return.
+    if (c == EOF || c == '\n') {
+      buffer[position] = '\n';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+
+    // If we have exceeded the buffer, reallocate.
+    if (position >= bufsize) {
+      bufsize += LSH_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer) {
+        fprintf(stderr, "bsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+}
+
+
+char **lsh_split_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+
+void print_prompt(void){
+    //char hostname[1024];
+    //char user[1024];
+    char* r;// = gethostname(hostname,1024);
+    /*if (r == -1){
+        err(-1,"gethostname");
+    }*/
+    r = getlogin();
+    /*if (r != 0){
+        err(-2,"getlogin_r");
+    }*/
+    //size_t size = strlen(hostname) + strlen(user); //add colors and a place to edit that 
+    //char *prompt_1 = strncat(hostname,"@",size);
+    //char *prompt_2 = strncat(prompt_1,user,size);
+    //red();
+    printf("\033[1;31m");
+    printf(r);
+    //reset();
+    printf("\033[0m");
+    printf("@");
+    //red();
+    printf("\033[1;31m");
+    printf("bsh");
+    //reset();
+    printf("\033[0m");
+    printf("~");
+}
+
+void loop(void){
+    //char *line;
+    char **args;
+    int status = 1;
+
+    do{
+        print_prompt();
+        
+        char *input = lsh_read_line();
+        struct Token *token = getParameters(input);  
+        args = lsh_split_line(input);
+        execvp(args[0],args);
+        int valid = valid_input(token);
+        status = valid;
+        if (valid){ //do not consider ./main -> to fix
+            args[0] = token->next->current_command.executable;
+            size_t byte_size = get_array_byte_size(args);
+            char **res = add_string_to_array(args,byte_size);
+            execute(res,token->next->current_command);
+        }
+    } while (status);
+
+}
+
+int main(void)
+{
+    loop();
+
+    return EXIT_SUCCESS;    
+}
