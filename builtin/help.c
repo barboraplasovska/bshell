2eb5cd3eea466fad1e2696a9e9a67c563b4604da
Barbora Plasovska <barbora.plasovska@epita.fr>
2022-04-10 13:10:37 +0200

    fixed help lol

--- builtin/help.c
+++ builtin/help.c
@@ -3,19 +3,36 @@
 int help(BuiltinFd *builtinFd)
 {
     fprintf(builtinFd->out, "Terminal powered by NaN (A1 S4)\n\n");
-    int f;
-    int count;
-    char buffer[BUFFER_SIZE*3]; 
-    f = open("../README", O_RDONLY);
-    while((count = read(f, buffer, sizeof(buffer))) > 0)
+    FILE* f;
+    char buffer[BUFFER_SIZE*3]; //characer buffer to store the bytes
+    f = fopen("./README", "r");
+    if(f == NULL)
+    {
+        fprintf(builtinFd->err, "cat: error: cannot open file");
+        exit(EXIT_FAILURE);
+        return -1;
+    }
+    while(fgets(buffer, BUFFER_SIZE, f))
     {
         fprintf(builtinFd->out, "%s", buffer);
     }
+    fclose(f);
 
     return 0;
 }
 
 int main()
 {
-	return 0;
+    struct builtinFd *terminal = NULL;
+    terminal = (struct builtinFd *) malloc(sizeof(struct builtinFd));
+    terminal->in = stdin;//STDIN_FILENO;
+    terminal->out = stdout;//(FILE *) STDOUT_FILENO;
+    terminal->err = stderr;//(FILE *) STDERR_FILENO;
+    terminal->inNo =  STDIN_FILENO;
+    terminal->outNo = STDOUT_FILENO;
+    terminal->errNo = STDOUT_FILENO;
+    int res = help(terminal);
+    free(terminal);
+    return res;
+
 }
